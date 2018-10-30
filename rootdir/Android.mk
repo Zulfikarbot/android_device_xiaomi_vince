2ea90f6d113dbfd9153d7ce915b0048c17fbb147
Oveno <ovenoboyo@gmail.com>
2018-10-30 17:10:42 +0530

    Zucc-ify: Add Zucc init scripts
    Signed-off-by: Oveno <ovenoboyo@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,3 +16,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+#Include Zucc init
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zuc.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.zuc.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
